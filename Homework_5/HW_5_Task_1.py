5f31b0df0745f2f304558b8ab4093ef670ef1c86
Oleg S <leshiyoleg@mail.ru>
2022-08-14 22:44:20 +0300

    Added hometask for Seminar_5 in README file, added Task_5_1 solution

---
+++ Homework_5/HW_5_Task_1.py
@@ -0,0 +1,34 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# ```
+# 'абвгдейка - это передача' = >" - это передача"
+# ```
+
+# функция находит в списке слова, содержащие определенный набор символов
+
+def find_word_in_text (list_to_find:list, word_to_find:str = 'абв') -> list:
+    result_list = []
+    for i, item in enumerate(list_to_find):
+        if len(item)>=len(word_to_find):
+            for j in range(len(item)-len(word_to_find)+1):
+                flag = False
+                if str(item[j:j+len(word_to_find)])==str(word_to_find):
+                    flag = True
+                    break
+            if flag == False:
+                result_list.append(item)
+        else:
+            result_list.append(item)
+    return result_list
+
+path = "Homework_5\example_task_1.txt"
+file = open (path, 'w', encoding = 'utf-8')
+data = file.write('абвгдейка - это передача')
+file.close()
+
+file = open(path, 'r', encoding='utf-8')
+data = file.read()
+list_to_find = data.split()
+file.close()
+
+print (" ".join(list_to_find))
+print(" ".join(find_word_in_text(list_to_find)))
